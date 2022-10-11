a5b8b339b7cbec4148fa2070a4a9f2c053ac2fe6
Iwá Duarte <iwaduarte@gmail.com>
2022-10-11 10:06:01 -0300

    feat: bcrypt with node 10+ support

--- template/package.json.js
+++ template/package.json.js
@@ -12,7 +12,7 @@
     "http-errors": "~1.6.3",
     "morgan": "~1.9.1",
     ${opts.compression ? `"compression":"~1.7.4",\n` : ''} ${opts.helmet ? `"helmet":"~6.0.0",\n` : ''} ${
-  opts.sequelize ? `"sequelize":"~6.24.0",\n"pg-hstore": "2.3.4",\n"pg":"8.8.0"\n` : ''
+  opts.sequelize ? `"sequelize":"~6.24.0",\n"pg-hstore": "2.3.4",\n"pg":"8.8.0"\n"bcrypt":"3.0.5"\n` : ''
 }  },
     "devDependencies": {
     "prettier": "^2.7.1"
