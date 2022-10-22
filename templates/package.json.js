4c578bd624bd650e2b3ac36374bee4b13a96e374
Iwá Duarte <iwaduarte@gmail.com>
2022-10-22 13:46:29 -0300

    feat: add nodemon and npm run dev

--- templates/package.json.js
+++ templates/package.json.js
@@ -5,6 +5,7 @@
   "private": true,
   "scripts": {
     "start": "node ./bin/www",
+    "dev": "nodemon ./bin/www",
     ${opts.sequelize ? `"update-db": "node ./database/syncDatabase"\n` : ''} },
   "dependencies": {
     "cookie-parser": "~1.4.4",
@@ -12,12 +13,13 @@
     "express": "~4.18.2",
     "http-errors": "~1.6.3",
     "morgan": "~1.9.1",
-    "dotenv":"16.0.3",
+    "dotenv": "16.0.3",
     ${opts.compression ? `"compression":"~1.7.4",` : ''}
     ${opts.helmet ? `"helmet":"~6.0.0",` : ''}
     ${opts.sequelize ? `"sequelize":"~6.24.0",\n"pg-hstore": "2.3.4",\n"pg":"8.8.0",\n"bcrypt":"5.1.0"` : ''}  },
     "devDependencies": {
-    "prettier": "^2.7.1"
+    "prettier": "^2.7.1",
+    "nodemon": "2.0.20" 
   }
 }
 `;
